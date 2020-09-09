352ccdaca51da07f0e0c126eacfe0ebe40dce3ed
yuchenliu <yuchenliu@synology.com>
2020-09-09 12:01:54 +0800

    Feat Take care array size

--- diskarray.cpp
+++ diskarray.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int parseFile(char *path, char *buf)
+int parseFile(char *path, char *buf, int size)
 {
     FILE *pFile = NULL;
     long FileSize = 0;
@@ -17,10 +17,7 @@ int parseFile(char *path, char *buf)
         printf("Read file failed: %s\n", path);
         goto fail;
     }
-    fseek(pFile, 0, SEEK_END);
-    FileSize = ftell(pFile);
-    rewind(pFile);
-    fread(buf, 1, FileSize, pFile);
+    fread(buf, 1, size, pFile);
     fclose(pFile);
     return 0;
 
@@ -31,11 +28,33 @@ fail:
     return -1;
 }
 
-Diskarray::Diskarray(string md) : mdName(md)
+uint64_t Diskarray::parseArraysize(void)
+{
+    if (this->level == "linear") {
+        this->ArraySize = this->minDiskSize * this->Disks.size();
+    }
+    else if (this->level == "raid1") {
+        this->ArraySize = this->minDiskSize;
+    }
+    else if (this->level == "raid5") {
+        this->ArraySize = this->minDiskSize * (this->Disks.size() - 1);
+    }
+    else if (this->level == "raid6") {
+        this->ArraySize = this->minDiskSize * (this->Disks.size() - 2);
+    }
+    else {
+        perror("non-supported level\n");
+        return 0;
+    }
+    return this->ArraySize;
+}
+
+Diskarray::Diskarray(string md) : mdName(md), minDiskSize(UINT64_MAX)
 {
     DIR *dir = NULL;
     struct dirent *ent = NULL;
     uint32_t offset = 0;
+    uint64_t disksize = 0;
     char slavesPath[128] = {0};
     char buffer[32] = {0};
     FILE *File = NULL;
@@ -49,11 +68,22 @@ Diskarray::Diskarray(string md) : mdName(md)
             memset(Path, 0, 128);
             memset(buffer, 0, 32);
             sprintf(Path, SYSFS_MD_DISK_OFFSET_FORMAT, md.c_str(), ent->d_name);
-            if (parseFile(Path, buffer) < 0) {
+            if (parseFile(Path, buffer, 32) < 0) {
                 continue;
             }
             sscanf(buffer, "%lu", &offset);
             this->Disks.push_back(Disk(ent->d_name, offset));
+
+            memset(Path, 0, 128);
+            memset(buffer, 0, 32);
+            sprintf(Path, SYSFS_MD_DISK_SIZE_FORMAT, md.c_str(), ent->d_name);
+            if (parseFile(Path, buffer, 32) < 0) {
+                continue;
+            }
+            sscanf(buffer, "%lu", &disksize);
+            if (this->minDiskSize > disksize) {
+                this->minDiskSize = disksize;
+            }
         }
         closedir (dir);
     }
@@ -64,7 +94,7 @@ Diskarray::Diskarray(string md) : mdName(md)
     memset(Path, 0, 128);
     memset(buffer, 0, 32);
     sprintf(Path, SYSFS_MD_CHUNK_SIZE_FORMAT, md.c_str());
-    if (parseFile(Path, buffer) < 0) {
+    if (parseFile(Path, buffer, 32) < 0) {
         goto fail;
     }
     sscanf(buffer, "%lu", &this->chunkSize);
@@ -72,20 +102,24 @@ Diskarray::Diskarray(string md) : mdName(md)
     memset(Path, 0, 128);
     memset(buffer, 0, 32);
     sprintf(Path, SYSFS_MD_LEVEL_FORMAT, md.c_str());
-    if (parseFile(Path, buffer) < 0) {
+    if (parseFile(Path, buffer, 32) < 0) {
         goto fail;
     }
     this->level = buffer;
+    this->level.erase(this->level.end() - 1);
 
+    this->parseArraysize();
 fail:
     return;
 }
 
-void Diskarray::PrintDiskarray()
+void Diskarray::PrintDiskarray(void)
 {
     vector<Disk>::iterator itrDisk;
     printf("md name: %s\n", this->mdName.c_str());
     printf("chunk size: %lu\n", this->chunkSize);
+    printf("minimum disk size: %llu\n", this->minDiskSize);
+    printf("Array size: %llu\n", this->ArraySize);
     printf("level: %s\n", this->level.c_str());
 
     printf("Disks:\n");
@@ -93,6 +127,42 @@ void Diskarray::PrintDiskarray()
         printf("\t");
         itrDisk->PrintDisk();
     }
+}
+
+int Diskarray::GetDiskSector(uint32_t sector, uint32_t *diskSector)
+{
+    if (this->level == "linear") {
+        return this->GetDiskSectorLinear(sector, diskSector);
+    }
+    else if (this->level == "raid1") {
+        return this->GetDiskSectorRaid1(sector, diskSector);
+    }
+    else if (this->level == "raid5") {
+        return this->GetDiskSectorRaid5(sector, diskSector);
+    }
+    else if (this->level == "raid6") {
+        return this->GetDiskSectorRaid6(sector, diskSector);
+    }
+    else {
+        perror("Non-supported raid level\n");
+        return -1;
+    }
+}
+
+int Diskarray::GetDiskSectorLinear(uint32_t sector, uint32_t *diskSector)
+{
+}
+
+int Diskarray::GetDiskSectorRaid1(uint32_t sector, uint32_t *diskSector)
+{
+}
+
+int Diskarray::GetDiskSectorRaid5(uint32_t sector, uint32_t *diskSector)
+{
+}
+
+int Diskarray::GetDiskSectorRaid6(uint32_t sector, uint32_t *diskSector)
+{
 }
 
 Disk::Disk(string name, uint32_t offset) : name(name), offset(offset)
--- diskarray.h
+++ diskarray.h
@@ -12,7 +12,10 @@ using namespace std;
 #define SYSFS_MD_CHUNK_SIZE_FORMAT "/sys/block/%s/md/chunk_size"
 #define SYSFS_MD_LEVEL_FORMAT "/sys/block/%s/md/level"
 #define SYSFS_MD_DISK_OFFSET_FORMAT "/sys/block/%s/md/dev-%s/offset"
+#define SYSFS_MD_DISK_SIZE_FORMAT "/sys/block/%s/md/dev-%s/size"
 #define SYSFS_MD_SLAVE_FORMAT "/sys/block/%s/slaves/"
+
+#define SECTOR_SIZE 512
 
 class Disk {
 public:
@@ -27,14 +30,25 @@ private:
 class Diskarray {
 public:
     Diskarray(string md);
-    void PrintDiskarray();
+
+    void PrintDiskarray(void);
+
+    uint64_t parseArraysize(void);
+
+    int GetDiskSector(uint32_t sector, uint32_t *diskSector);
+    int GetDiskSectorLinear(uint32_t sector, uint32_t *diskSector);
+    int GetDiskSectorRaid1(uint32_t sector, uint32_t *diskSector);
+    int GetDiskSectorRaid5(uint32_t sector, uint32_t *diskSector);
+    int GetDiskSectorRaid6(uint32_t sector, uint32_t *diskSector);
 
 private:
     string mdName;
     vector<Disk> Disks;
     uint32_t chunkSize;
+    uint64_t minDiskSize;
+    uint64_t ArraySize;
     string level;
 };
 
-int parseFile(char *, char *);
+int parseFile(char *, char *, int);
 #endif
